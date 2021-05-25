cb88c4eed3fdfe7c30907e019d7d07a059577226
Thiago Gama <thiagodavid035@gmail.com>
2021-05-25 18:01:07 -0300

    iniciação --Thiago

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get(`/`, (req, res, next) => {
 //     next();
 // }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3013;
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
